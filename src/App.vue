2c2bdaf14cb7cb3ea56269be707a795825ac5487
Sander Moolin <sandermoolin@gmail.com>
2019-07-05 23:53:27 -0400

    Basic shader up and running

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <FragmentShader class="shader" :key="Date.now()">
+            <script type="shader/fragment">
+                precision mediump float;
+                uniform vec2 uResolution;
+                uniform float uTime;
+                void main() {
+                    vec2 uv = gl_FragCoord.xy / uResolution.xy;
+                    gl_FragColor = vec4(uv.x, 0.2, 0.2, 1.);
+                }
+            </script>
+        </FragmentShader>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FragmentShader from '@fuzzco/fragment-shader'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    components: {
+        FragmentShader
+    }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+
+    .shader {
+        width: 66vw;
+        height: 66vh;
+        position: relative;
+    }
 }
 </style>
